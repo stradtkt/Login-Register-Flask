d1f41a9ef80ca60052374b75cbc708f498baf28e
Kevin <stradtkt22@gmail.com>
2018-06-29 13:59:15 -0400

    Change to md5 instead of bcrypt and it is logging the user in now

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from flask import Flask, request, redirect, render_template, session, flash, url_for
 from mysqlconnection import MySQLConnector
-from flask.ext.bcrypt import Bcrypt
+import md5
 
 app = Flask(__name__)
 app.secret_key = "dfdfdsfsd.sdf.fsdfg.g.tedg.dt.gdf.gfd.!"
-mysql = MySQLConnector(app, 'airbnb')
-bcrypt = Bcrypt(app)
+mysql = MySQLConnector(app, 'login')
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -18,35 +17,67 @@ def login_now():
 def register_now():
   	return render_template('register.html')
 
+
 @app.route('/register', methods=['POST'])
 def register():
   	valid = True
   	print request.form
 	if request.form['email'] == "":
   		valid = False
-  		flash("Email cannot be empty")
+  		flash("Email cannot be empty", 'danger')
 	if request.form['name'] == "":
   		valid = False
-  		flash("Name cannot be empty")
+  		flash("Name cannot be empty", 'danger')
 	if request.form['password'] == "":
   		valid = False
-    	flash("Password cannot be empty")
+    	flash("Password cannot be empty", 'danger')
+	if request.form['password'] == request.form['confirm_password']:
+  		valid = True
+		flash('Passwords Match!', 'success')
 	if valid != True:
   		return redirect('/')
 	else:
-  		query = "INSERT INTO `airbnb`.`users` (`email`, `password`, `name`, `created_at`, `updated_at`) VALUES (:email, :password, :name, now(), now());"
+  		query = "INSERT INTO `login`.`users` (`email`, `password`, `name`, `created_at`, `updated_at`) VALUES (:email, :password, :name, now(), now());"
 		data = {
 			"email": request.form['email'],
-			"password": bcrypt.generate_password_hash(request.form['password']),
+			"password": md5.new(request.form['password']).hexdigest(),
 			"name": request.form['name']
 		}
 		mysql.query_db(query, data)
-		flash("Successfully Registered. Login now")
+		flash("Successfully Registered. Login now", 'success')
 		return redirect(url_for('login_now'))
   	return "got registered"
 
 
 @app.route('/login', methods=['POST'])
 def login():
-  	return "got logged in"
+    email = request.form['email']
+    password = request.form['password']
+    valid = True
+    if request.form['email'] == "":
+        valid = False
+        flash("Email cannot be empty", 'danger')
+    if request.form['password'] == "":
+        valid = False
+        flash("Password cannot be empty", 'danger')
+    if not valid:
+        return redirect("/")
+    else:
+        query = "SELECT * FROM users WHERE email = :email AND password = :password"
+        data = {
+            "email":request.form['email'],
+            "password": md5.new(request.form['password']).hexdigest()
+        }
+        user = mysql.query_db(query, data)
+        if len(user) != 0:
+            session['id'] = user[0]['id']
+            session['name'] = user[0]['name']
+            return redirect(url_for('dashboard'))
+    return redirect('/')
+	
+@app.route('/dashboard')
+def dashboard():
+  	return render_template('dashboard.html')
+
+
 app.run(debug=True)
--- mysqlconnection.py
+++ mysqlconnection.py
@@ -8,11 +8,11 @@ class MySQLConnection(object):
                 'host': 'localhost',
                 'database': db, # we got db as an argument
                 'user': 'root',
-                'password': 'root',
+                'password': 'Buster183845',
                 'port': '3306' # change the port to match the port your SQL server is running on
         }
         # this will use the above values to generate the path to connect to your sql database
-        DATABASE_URI = "mysql://{}:{}@127.0.0.1:{}/{}".format(config['user'], config['password'], config['port'], config['database'])
+        DATABASE_URI = "mysql+pymysql://{}:{}@127.0.0.1:{}/{}".format(config['user'], config['password'], config['port'], config['database'])
         app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
         app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
         # establish the connection to database
